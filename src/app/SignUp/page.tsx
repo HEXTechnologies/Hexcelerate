f9c8b33e5d748bfb68866f066fd5be1d02921cf7
hoangv11 <victorhoang11@gmail.com>
2025-01-28 18:01:02 -1000

    Redesign login and signup page

--- src/app/SignUp/page.tsx
+++ src/app/SignUp/page.tsx
@@ -1,12 +1,65 @@
-import SignUpForm from "../../components/UserLogin/SignUpForm";
-import "./SignUpPage.css";
+"use client";
 
-export default function Home() {
+import React, { useRef } from "react";
+import { ArrowLeft } from "lucide-react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Link from "next/link";
+import ReCAPTCHA from "react-google-recaptcha";
+
+export default function Waitlist() {
+  const recaptchaRef = useRef<ReCAPTCHA | null>(null);
+
   return (
-    <main className="home-container">
-      <div className="form-wrapper">
-        <SignUpForm />
+    <div className="min-h-screen bg-black py-5">
+      <div className="container">
+        <Link
+          href="/"
+          className="text-white mb-5"
+          style={{ display: "flex", alignItems: "center" }}
+        >
+          <ArrowLeft size={20} className="mr-2" />
+        </Link>
+
+        <div className="row justify-content-center">
+          <div className="col-12 col-md-8 col-lg-6">
+            <div
+              className="card bg-black border border-blue-800"
+              style={{ borderRadius: "1rem" }}
+            >
+              <div className="card-body p-4">
+                <div className="text-center mb-4">
+                  <h2 className="text-3xl font-bold text-white">
+                    Create Your Account
+                  </h2>
+                  <p className="text-white mt-2">
+                    Enter your information to get started.
+                  </p>
+                </div>
+
+                <div className="mb-4 d-flex justify-content-center">
+                  <ReCAPTCHA
+                    ref={recaptchaRef}
+                    sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || ""}
+                    theme="dark"
+                  />
+                </div>
+
+                <div className="d-flex justify-content-center">
+                  <button
+                    type="submit"
+                    className="btn btn-primary w-50 bg-blue border-white"
+                  >
+                    Sign Up
+                  </button>
+                </div>
+                <p className="text-center mt-3 text-white">
+                  Already have an account? <a href="SignIn">Sign In</a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </main>
+    </div>
   );
 }
