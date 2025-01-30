6b0df81308ba29ba814ed5635ddee277665daff1
hoangv11 <victorhoang11@gmail.com>
2025-01-29 16:42:29 -1000

    Update UserLogin.tsx

--- src/components/UserLogin.tsx
+++ src/components/UserLogin.tsx
@@ -5,3 +5,9 @@ import { auth, firestore } from "../../firebaseConfig/firebase";
 import React, { useState, useRef } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import Swal from "sweetalert2";
+
+const RegisterAccount = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+};
