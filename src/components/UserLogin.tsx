328254bba61d926cb3609cc7d07a130bd3b79680
hoangv11 <victorhoang11@gmail.com>
2025-01-29 16:35:14 -1000

    Create UserLogin.tsx

---
+++ src/components/UserLogin.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { doc, setDoc, Timestamp } from "firebase/firestore";
+import { auth, firestore } from "../../firebaseConfig/firebase";
+import React, { useState, useRef } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import Swal from "sweetalert2";
