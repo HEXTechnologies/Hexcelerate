87039e83f64070858dff8ec9f05b2a804be68f8d
hoangv11 <victorhoang11@gmail.com>
2025-02-02 17:57:29 -1000

    Update page.tsx

---
+++ src/app/Register/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React, {useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Router from "next/router";
+
+export default function Register() {
+    return (
+        
+    );
+}
