dfcb96e7832aa98719b625b578899617fc968649
hoangv11 <victorhoang11@gmail.com>
2025-02-02 18:06:47 -1000

    Update page.tsx

--- src/app/Register/page.tsx
+++ src/app/Register/page.tsx
@@ -3,6 +3,8 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useRouter } from "next/navigation";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
 
 export default function Register() {
   const [selectedRole, setSelectedRole] = useState("");
@@ -17,9 +19,29 @@ export default function Register() {
   };
 
   return (
-    <div>
-      <h1>Sign up to unlock the full experience</h1>
-      <p>You will have access to all features</p>
+    <div className="h-screen bg-black flex items-center justify-center py-5">
+      <div className="container">
+        <Link
+          href="/HomePage"
+          className="text-white mb-5 flex items-center"
+          style={{ display: "flex", alignItems: "center" }}
+        >
+          <ArrowLeft size={20} className="mr-2" />
+        </Link>
+
+        <div className="row justify-content-center">
+          <div className="col-12 col-md-8 col-lg-6">
+            <div
+              className="card bg-black border border-blue-800"
+              style={{ borderRadius: "1rem" }}
+            >
+              <div className="card-body p-4">
+                <div className="mb-4 d-flex justify-content-center"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
