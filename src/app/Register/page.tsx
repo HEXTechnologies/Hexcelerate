268fbce7a54aa43e3967338312b48fc9532877ad
hoangv11 <victorhoang11@gmail.com>
2025-02-02 18:02:42 -1000

    Update page.tsx

--- src/app/Register/page.tsx
+++ src/app/Register/page.tsx
@@ -1,18 +1,20 @@
 "use client";
 
-import React, {useState } from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useRouter } from "next/router";
 
 export default function Register() {
-    const [selectedRole, setSelectedRole] = useState("");
-    const router = useRouter();
+  const [selectedRole, setSelectedRole] = useState("");
+  const router = useRouter();
 
-    const handleRoleSelection = (role: any) => {
-        setSelectedRole(role);
-    };
-    
-    return (
-        
-    );
+  const handleRoleSelection = (role: any) => {
+    setSelectedRole(role);
+  };
+
+  const handleSignUp = () => {
+    router.push("/SignUp?selectedRole=${encodeURIComponent(selectedRole)}");
+  };
+
+  return <div></div>;
 }
