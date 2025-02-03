ea0dc08ca5a817bf769635c0446f93544c1a0f47
hoangv11 <victorhoang11@gmail.com>
2025-02-02 17:48:40 -1000

    Update PreseedNavbar.tsx

--- src/components/HomePageComponents/PreseedNavbar.tsx
+++ src/components/HomePageComponents/PreseedNavbar.tsx
@@ -301,11 +301,11 @@ const PreseedNavbar: React.FC<NavbarProps> = ({
               </li> */}
               <li className="nav-item mb-2">
                 <Link
-                  href="SignUp"
+                  href="SignIn"
                   className="btn btn-primary gradient-button text-white px-4 py-2"
                   style={{ fontSize: "0.9rem" }}
                 >
-                  Sign Up
+                  Sign In
                 </Link>
               </li>
             </ul>
